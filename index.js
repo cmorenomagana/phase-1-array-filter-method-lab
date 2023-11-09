c5e0b53bb921c15e64a386e6ae1e8db8c6efcd74
Cuitlahuatl <cmorenomagana@gmail.com>
2023-11-09 11:52:15 -0600

    Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((drivers) => drivers.toLowerCase().includes(name.toLowerCase()))
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((drivers)=> drivers.slice(0, 2) === letters)
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((drivers)=> drivers.name === name)
+}
